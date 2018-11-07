84f2c15442d2b6622a8d37bdee186279576735a7
easwar1213 <35298840+easwar1213@users.noreply.github.com>
2018-11-07 12:17:15 +0530

    Commit

--- src/AssetSPA/Assets.js
+++ src/AssetSPA/Assets.js
@@ -3,20 +3,88 @@ import React from 'react';
 import { BooleanInput, SimpleShowLayout, SimpleList, ReferenceManyField, ReferenceArrayField, SelectArrayInput, Show, ShowButton, Tab, TabbedShowLayout, Filter, List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'react-admin';
 
 import Map from './map';
-import SensorGroupTable from './SensorGroupTable'
+import SensorGroupTable from './SensorGroupTable';
+import Table from '../components/table/Table';
+import Panel from '../components/Panel';
 
 
 export const AssetList = (props) => (
-    <List title="Assets" {...props} filters={<AssetFilter />}  >
-        <Datagrid>
-            <TextField source="telematicsSerialNumber" />
-            <TextField source="make" />
-            <TextField source="assetName" />
-            <TextField source="model" />
-            <TextField source="distributorName" />
-            <ShowButton />
-        </Datagrid>
-    </List>
+    // <List title="Assets" {...props} filters={<AssetFilter />}  >
+    //     <Datagrid>
+    //         <TextField source="telematicsSerialNumber" />
+    //         <TextField source="make" />
+    //         <TextField source="assetName" />
+    //         <TextField source="model" />
+    //         <TextField source="distributorName" />
+    //         <ShowButton />
+    //     </Datagrid>   
+    // </List>
+
+    <Panel lg={12} title="Device Status">
+        <Table responsive className='table--bordered'>
+          <thead>
+          <tr>            
+            <th>Telematics Serial#</th>
+            <th>Make</th>
+            <th>Asset Name</th>
+            <th>Model</th>
+            <th>Distributor Name</th>            
+            <th>Asset Details</th>            
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>1141233567</td>
+            <td>Compressor</td>
+            <td>LS 110</td>
+            <td>Asset 3</td>
+            <td>Distributor1</td>
+            <td>Show</td>           
+          </tr>
+          <tr>
+            <td>1212343457</td>
+            <td>Compressor</td>
+            <td>LS 90</td>
+            <td>Asset 1</td>
+            <td>Distributor2</td>
+            <td>Show</td>                        
+          </tr>
+          <tr>
+            <td>1212343458</td>
+            <td>Compressor</td>
+            <td>LS 110</td>
+            <td>Asset 1</td>
+            <td>Distributor2</td>
+            <td>Show</td>                       
+          </tr>
+          <tr>
+            <td>1212123434</td>
+            <td>Compressor</td>
+            <td>LS 110</td>
+            <td>Asset 2</td>
+            <td>Distributor 3</td>
+            <td>Show</td>                           
+          </tr>
+          <tr>
+            <td>1121234345</td>
+            <td>Compressor</td>
+            <td>LS 110</td>
+            <td>Asset 2</td>
+            <td>Distributor 3</td>
+            <td>Show</td>                          
+          </tr>
+          <tr>
+            <td>1231212343</td>
+            <td>Compressor</td>
+            <td>LS 110</td>
+            <td>Asset 3</td>
+            <td>Distributor1</td>
+            <td>Show</td>                      
+          </tr>
+          </tbody>
+        </Table>
+      </Panel>
+    
 );
 
 const AssetTitle = ({ record }) => {
